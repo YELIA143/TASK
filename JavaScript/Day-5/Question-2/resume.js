49fd76db753324110747730f0d203648ebe22f55
YELIA143 <142712567+YELIA143@users.noreply.github.com>
2023-11-26 19:46:58 +0530

    Add files via upload

---
+++ JavaScript/Day-5/Question-2/resume.js
@@ -0,0 +1,52 @@
+let myResume={
+    "basics": {
+      "Name": "YELIA B",
+      "age" : 26,
+      "sex": "Male",
+      "email": "eliyabandaru@gamil.com",
+      "phone": 8637447957,
+      "degree": "B.COM",
+      "location": {
+        "address": "23 mariamman kovil street kaylanapuram Ambattur",
+        "postalCode": 600053,
+        "city": "Chennai",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+    },
+    "work": [
+      {
+        "company or other": "Mother's clinic",
+        "position": "maintenance",
+        "date of join": "2019-11-04 and still working", 
+      },
+    ],
+    "education": [
+      {
+        "institution": "Vel Tech Rangarajan Dr.Sagunthala R&D Institute of Science and technology",
+        "department": "General",
+        "studyType": "fulltime",
+        "batch start year": 2016,
+        "batch end year": 2019,
+        "gpa": 6.32,
+      }
+    ],
+    "technical skills": [
+      {
+        "name": "Ms offer, photography, javascript, html, css",
+        "level": "beginer",
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish,Telugu",
+      }
+    ],
+    "hobbies": [
+      {
+        "name": "listening Music, Playing chess, Playing Cricket",
+      }
+    ]
+  }
+  console.log(myResume);
+  
